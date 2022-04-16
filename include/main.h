6bf388c078d3357f94b7bf43faed7939935c7838
Missingmew <nintendo6496@googlemail.com>
2022-04-16 22:24:49 +0200

    fixup size of roomdata and related dumped data

--- include/main.h
+++ include/main.h
@@ -90,8 +90,7 @@ struct Main
     /* +0x0DC */ u32 gameStateFlags; // unity: status_flag matches debug menu
     /* +0x0E0 */ u32 talkEndFlags[8]; // unity: talk_end_flag // TODO: find right size
     /* +0x100 */ u32 unk100[8]; // script related, apollo's FW_Mess_flag??
-    /* +0x120 */ u8 roomData[16][8]; // unity: Map_data //TODO: first size might be wrong
-    u8 filler1A0[0x1A4 - 0x1A0];
+    /* +0x120 */ u8 roomData[26][5]; // unity: Map_data //TODO: first size might be wrong
     /* +0x1A4 */ struct PsycheLock unk1A4[4];
     u8 filler244[0x254 - 0x244];
     /* +0x254 */ u32 soundFlags;
